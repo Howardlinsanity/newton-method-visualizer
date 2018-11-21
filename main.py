ff61e8cf3efd7704c225ff6122539ee3b3c774d4
Howard <linnyflow@gmail.com>
2018-11-20 16:21:01 -0800

    Adding pyplot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from scipy import misc 
 from math import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 '''
     Calculates Delta of the function, which is the error
@@ -21,21 +23,22 @@ def derivative(f,x):
     return misc.derivative(f, x, dx=1e-12)
 
 '''
-    Newton's Method - currently made this recursive due to simplicity, may need to convert to iterative in future
+    Newton's Method 
     f: function that takes in an x
     xi: value (x0 when initially called)
     e: Maximum error (best if under 11 decimal places)
 '''
 def newtons_method(f,xi,e):
     delta = dx(f,xi) 
-    if(delta < e):
-        return xi 
-    else:
+    while(delta > e):
         df = derivative(f,xi)
         xi = xi - (f(xi)/df)
         delta = dx(f,xi)
-        return newtons_method(f,xi,e)
+    return xi
 
+'''
+    Will ask user for a function and then return said function as well as string representation
+'''
 def function_parser():
     safe_list = ['acos', 'asin', 'atan', 'atan2', 'ceil', 'cos', 'cosh', 'degrees', 'e','exp','fabs', 'floor', 'fmod','frexp','hypot','ldexp', 'log', 'log10','modf', 'pi','pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan','tanh']
     safe_dict = dict([(k,locals().get(k,None)) for k in safe_list]) 
@@ -43,10 +46,21 @@ def function_parser():
     def f(x):
         safe_dict['x'] = x
         return eval(expr, {"__builtins__":None},safe_dict)
-    return f
+    return (f, expr)
 
 if __name__ == "__main__":
-    func = function_parser()
-    x0s = [0,0.5,1]
-    for x in x0s:
-        print("Root: ", round(newtons_method(func,x,1e-5),4))
+    (func, expr) = function_parser()
+    initial = int(input("Input initial point please: "))
+    root = round(newtons_method(func, initial,1e-11),4)
+    print("Root: ", root)
+    x = np.arange(-2+root, 2+root, 0.01)
+    yvals = func(x)
+    plt.plot(x, yvals)
+    plt.axis([-2.0+root, 2.0+root, -4.0, 4.0])
+    plt.axhline(0, color='black')
+    plt.axvline(0, color='black')
+    plt.title('Newtons Method')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.legend(['f(x) = '+ expr])
+    plt.show()
